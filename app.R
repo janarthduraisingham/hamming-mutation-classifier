35db74bdda6c30d1201a75cf5864e97b493dcaa5
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-03-13 11:16:59 +0000

    add reactive values object for print messages

--- app.R
+++ app.R
@@ -14,6 +14,13 @@ ui <- page_fluid(
     
     actionButton("run", "Run")
     
+  ),
+  
+  card(
+    card_header("Results"),
+    
+    textOutput("run_complete"),
+    textOutput("test_sequence")
   )
 )
 
@@ -23,14 +30,31 @@ server <- function(input, output) {
   source("R/hypothesis_test.R")
   source("R/p_value.R")
   
-  test_sequence <- 'AAAAAAAA'
-           
-  reference_sequence <- 'ATGCATGC'
+  reactive_values <- reactiveValues(run_complete = '')
   
+  reactive_values$test_sequence <- 'AAAAAAAA'
+           
+  reactive_values$reference_sequence <- 'ATGCATGC'
+  
+  #reactive_values$run_complete <- ''
+  
+  # Initialise print messages
+  output$test_sequence = renderText(paste0("Test sequence: ", reactive_values$test_sequence))
+  output$run_complete = renderText(paste0(reactive_values$run_complete))
+  
+  
+  # Run Button
   observeEvent(input$run,
-               hypothesis_test(test_sequence = 'AAABBBBB',
-                  reference_sequence = 'AAAAAAAA',
+               {
+               # Run hypothesis test
+               hypothesis_test(test_sequence = reactive_values$test_sequence,
+                  reference_sequence = reactive_values$reference_sequence,
                   hamming_distance_function = hamming_distance)
+               
+               # Print message
+               reactive_values$run_complete = 'Hypothesis test complete'
+               
+               }
   )
   
 }
