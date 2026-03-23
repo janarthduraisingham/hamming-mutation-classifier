8aef77fc26a10aa4e5abb3d8f138ba73fb4e0980
janarthduraisingham <51697690+janarthduraisingham@users.noreply.github.com>
2026-03-23 17:41:03 +0000

    Feature/003 input reference (#40)
    * add input widget for reference sequence
    
    * edit app title
    
    * implement reference sequence input

--- app.R
+++ app.R
@@ -3,14 +3,19 @@ library(bslib)
 
 ui <- page_fluid(
   
-  titlePanel("Hamming Distance Mutation Classifier"),
+  titlePanel("Hamming Distance Mutation Hypothesis Test"),
   
   card(
     card_header("Setup"),
     
     textInput("test_sequence",
               "Test Sequence",
-              placeholder = "e.g ATCGATCG")
+              placeholder = "e.g. ATCGATCG"),
+    
+    textInput("reference_sequence",
+              "Reference sequence",
+              placeholder = "e.g. ATCGATCG")
+    
   ),
   
   card(
@@ -69,7 +74,7 @@ server <- function(input, output) {
   #test_sequence <- 'AAAAAAAA'
   #test_sequence = input$test_sequence
            
-  reference_sequence <- 'ATGCATGC'
+  #reference_sequence <- 'ATGCATGC'
   
   distribution = 'binomial'
   single_mutation_probability = 0.1
@@ -117,7 +122,7 @@ server <- function(input, output) {
                
                # Run hypothesis test
                hypothesis_test = hypothesis_test(test_sequence = input$test_sequence,
-                                         reference_sequence = reference_sequence,
+                                         reference_sequence = input$reference_sequence,
                                          hamming_distance_function = hamming_distance,
                                          distribution = distribution,
                                          single_mutation_probability = single_mutation_probability)
@@ -143,7 +148,7 @@ server <- function(input, output) {
                
                reactive_values$test_sequence = input$test_sequence
 
-               reactive_values$reference_sequence = reference_sequence
+               reactive_values$reference_sequence = input$reference_sequence
                
                reactive_values$h0 = paste0("The Hamming distance between the test sequence and the reference sequence follows a ", distribution, " distribution with single independent mutation probability, ", single_mutation_probability, ", and ", nchar(input$test_sequence), " trials.")
                
