a97fd343555834b26cd451458af0cd587e377228
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-03-24 13:57:36 +0000

    uncomment forcing of tick marks

--- R/hamming_distance_distribution.R
+++ R/hamming_distance_distribution.R
@@ -16,11 +16,9 @@ hamming_distance_distribution_plotter <- function(trials,
     labs(title = "Hamming Distance Probability Distribution",
          subtitle = paste0("Trials: ", trials, "\nSingle mutation probability: ", single_mutation_probability),
          x = "Hamming distance",
-         y = "Probability")# +
-    #scale_x_continuous(breaks = seq(0, trials, by = 1)) #+
-    #scale_y_continuous(breaks = seq(0, 1, by = 0.05))
-  
-  
+         y = "Probability") +
+    scale_x_continuous(breaks = seq(0, trials, by = 1)) +
+    scale_y_continuous(breaks = seq(0, 1, by = 0.05))
 }
 
 
