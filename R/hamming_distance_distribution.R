3d7fe295c6b4b7b7e28e43aa1bfa0f4daa45e671
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-03-24 13:59:36 +0000

    update x axis label

--- R/hamming_distance_distribution.R
+++ R/hamming_distance_distribution.R
@@ -15,7 +15,7 @@ hamming_distance_distribution_plotter <- function(trials,
     theme_classic() +
     labs(title = "Hamming Distance Probability Distribution",
          subtitle = paste0("Trials: ", trials, "\nSingle mutation probability: ", single_mutation_probability),
-         x = "Hamming distance",
+         x = "Hamming distance (Number of mutations)",
          y = "Probability") +
     scale_x_continuous(breaks = seq(0, trials, by = 1)) +
     scale_y_continuous(breaks = seq(0, 1, by = 0.05))
