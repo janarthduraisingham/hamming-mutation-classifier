4d436abf254ce7e7df87b145478302ed25b66a79
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-03-24 14:18:19 +0000

    edit template to get cumulative distribution

--- R/hamming_distance_cumulative_distribution.R
+++ R/hamming_distance_cumulative_distribution.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 
-hamming_distance_distribution_plotter <- function(trials,
+hamming_distance_cumulative_distribution_plotter <- function(trials,
                                                   single_mutation_probability){
   
   x = seq(0, trials)
-  y = dbinom(x,
+  y = pbinom(x,
              size = trials,
              prob = single_mutation_probability)
   
@@ -13,10 +13,10 @@ hamming_distance_distribution_plotter <- function(trials,
   ggplot(df, aes(x = mutations, y = probability)) +
     geom_bar(stat = 'identity', position = 'dodge') +
     theme_classic() +
-    labs(title = "Hamming Distance Probability Distribution",
+    labs(title = "Hamming Distance Cumulative Probability Distribution",
          subtitle = paste0("Trials: ", trials, "\nSingle mutation probability: ", single_mutation_probability),
          x = "Hamming distance (Number of mutations)",
-         y = "Probability") +
+         y = "Cumulative Probability") +
     scale_x_continuous(breaks = seq(0, trials, by = 1)) +
     scale_y_continuous(breaks = seq(0, 1, by = 0.05))
 }
