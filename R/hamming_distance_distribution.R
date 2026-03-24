57b16c01ff00eccaf0676cc5555b313e8cdd717b
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-03-24 13:11:53 +0000

    refactor code for hamming distance distribution plotter

--- R/hamming_distance_distribution.R
+++ R/hamming_distance_distribution.R
@@ -1,17 +1,29 @@
 library(tidyverse)
 
-n = 8
-p = 0.9
-x = seq(1:n)
-y = dbinom(x,
-           size = n,
-           prob = p)
-df = data.frame(list(mutations = x, probability = y))
+hamming_distance_distribution_plotter <- function(trials,
+                                                  single_mutation_probability){
+  
+  x = seq(1:n)
+  y = dbinom(x,
+             size = trials,
+             prob = single_mutation_probability)
+  
+  df = data.frame(list(mutations = x, probability = y))
+  
+  ggplot(df, aes(x = mutations, y = probability)) +
+    geom_bar(stat = 'identity', position = 'dodge') +
+    theme_classic() +
+    labs(title = "Hamming Distance Probability Distribution",
+         subtitle = paste0("Trials: ", trials, "\nSingle mutation probability: ", single_mutation_probability),
+         x = "Hamming distance",
+         y = "Probability")
+  
+  
+}
 
-ggplot(df, aes(x = mutations, y = probability)) +
-  geom_bar(stat = 'identity', position = 'dodge') +
-  theme_classic() +
-  labs(title = "Hamming dist dist",
-       subtitle = "subtitle",
-       x = "Hamming distance",
-       y = "Probability")
+hamming_distance_distribution_plotter(trials = 8,
+                                      single_mutation_probability = 0.1)
+
+
+  
+  
