9631537d47af73747c3651a46919e271a6d5fdc9
janarthduraisingham <51697690+janarthduraisingham@users.noreply.github.com>
2026-03-25 10:21:25 +0000

    remove x = 9 on p value graph (#52)

--- R/hamming_distance_p_value_graph.R
+++ R/hamming_distance_p_value_graph.R
@@ -18,7 +18,7 @@ hamming_distance_p_value_plotter <- function(trials,
          subtitle = paste0("Trials (length of sequence): ", trials, "\nSingle mutation probability: ", single_mutation_probability),
          x = "Hamming distance (Number of mutations)",
          y = "1 - Cumulative Probability") +
-    scale_x_continuous(breaks = seq(0, trials+1, by = 1)) +
+    scale_x_continuous(breaks = seq(0, trials, by = 1)) +
     scale_y_continuous(breaks = seq(0, 1, by = 0.05)) +
     geom_hline(yintercept = 1-threshold, color = "red")
 }
